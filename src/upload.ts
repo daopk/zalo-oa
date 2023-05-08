6942dcbc53f4458387d8aa267966a53c1ab5a7bf
Phan Khắc Đạo <daopk.dev@gmail.com>
2023-05-08 07:54:16 +0700

    Add upload apis

--- src/upload.ts
+++ src/upload.ts
@@ -1,3 +1,40 @@
-// TODO: Add upload function
+import type { ZaloOAResponse } from './types/response'
+import { zfetch } from './zfetch'
 
-export {}
+const baseURL = 'https://openapi.zalo.me/v2.0/oa/upload'
+
+export async function uploadImage(access_token: string, file: Blob | File) {
+  const data = new FormData()
+  data.append('file', file)
+
+  return zfetch<ZaloOAResponse<{ attachment_id: string }>>('image', {
+    method: 'POST',
+    baseURL,
+    data,
+    headers: { access_token },
+  })
+}
+
+export async function uploadGif(access_token: string, file: Blob | File) {
+  const data = new FormData()
+  data.append('file', file)
+
+  return zfetch<ZaloOAResponse<{ attachment_id: string }>>('gif', {
+    method: 'POST',
+    baseURL,
+    data,
+    headers: { access_token },
+  })
+}
+
+export async function uploadFile(access_token: string, file: Blob | File) {
+  const data = new FormData()
+  data.append('file', file)
+
+  return zfetch<ZaloOAResponse<{ token: string }>>('file', {
+    method: 'POST',
+    baseURL,
+    data,
+    headers: { access_token },
+  })
+}
