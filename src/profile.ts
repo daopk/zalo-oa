77b2824d97062417fc9d9eeee417cb321196463e
Phan Khắc Đạo <daopk.dev@gmail.com>
2023-05-16 23:20:22 +0700

    Remove unnecessary `async` keyword

--- src/profile.ts
+++ src/profile.ts
@@ -3,7 +3,7 @@ import type { GetFollowersResponse, GetOAResponse, GetProfileResponse } from './
 
 const baseURL = 'https://openapi.zalo.me/v2.0/oa'
 
-export async function getUserProfile(access_token: string, user_id: string) {
+export function getUserProfile(access_token: string, user_id: string) {
   const params = { data: JSON.stringify({ user_id }) }
 
   return zfetch<GetProfileResponse>('getprofile', {
@@ -13,14 +13,14 @@ export async function getUserProfile(access_token: string, user_id: string) {
   })
 }
 
-export async function getOAProfile(access_token: string) {
+export function getOAProfile(access_token: string) {
   return zfetch<GetOAResponse>('getoa', {
     baseURL,
     headers: { access_token },
   })
 }
 
-export async function getFollowers(access_token: string, offset = 0, count = 50, tag_name?: string) {
+export function getFollowers(access_token: string, offset = 0, count = 50, tag_name?: string) {
   const params = { data: JSON.stringify({ offset, count, tag_name }) }
   return zfetch<GetFollowersResponse>('getfollowers', {
     baseURL,
